643f157dc3a3968795379d949d7df5f0f136e3e7
mahmoud adil <mahmod.adil2001@gmail.com>
2025-07-24 09:33:16 +0300

    Create test123.py

---
+++ test123.py
@@ -0,0 +1,12 @@
+title = "Lecture Test 123"
+
+slides = [
+    {
+        "title": "Test Slide",
+        "subtitle": "Test Subtitle",
+        "content": [
+            "This is a test point.",
+            "Second point to test uniqueness."
+        ]
+    }
+]
